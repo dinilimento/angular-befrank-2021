76bdbbadfdde30aa2f81e48d88ddce8eaeb0b75f
JP <jp@jp.nl>
2021-12-14 11:21:24 +0100

    FormBuilder gebruikt

--- demo/src/app/pages/forms/forms.page.ts
+++ demo/src/app/pages/forms/forms.page.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { TForm } from 'src/app/typed-forms';
 import { Person } from '../../models/person';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.page.html',
-  styleUrls: ['./forms.page.css']
+  styleUrls: ['./forms.page.css'],
 })
 export class FormsPage implements OnInit {
-  form: TForm<Person> = new FormGroup({
-    name: new FormControl(undefined, [Validators.required, Validators.pattern('^[a-zA-Z -]+$')]),
-    birthDate: new FormControl(undefined, Validators.required),
-    photoUrl: new FormControl(undefined, Validators.required)
-  }) as TForm<Person>;
+  form!: TForm<Person>;
 
   newPerson = {} as Person;
 
@@ -21,12 +22,20 @@ export class FormsPage implements OnInit {
     return this.form.controls;
   }
 
+  constructor(private fb: FormBuilder) {} // DI
+
   ngOnInit(): void {
+    this.form = this.fb.group({
+      name: [, [Validators.required, Validators.pattern('^[a-zA-Z -]+$')]],
+      birthDate: [, Validators.required],
+      photoUrl: [, Validators.required],
+    }) as TForm<Person>;
+
     setTimeout(() => {
       this.form.setValue({
         name: 'JP',
         birthDate: '2004-07-14',
-        photoUrl: 'bla.jpg'
+        photoUrl: 'bla.jpg',
       });
     }, 2000);
   }
